34fe2ef1d871a5ba91dfaf15f0222d2140d64843
Csongor Zalatnai <csongor@zalatnai.hu>
2015-11-06 21:25:11 +0100

    fixed plugin registration

--- www/push.js
+++ www/push.js
@@ -3,7 +3,7 @@ var exec = cordova.require('cordova/exec');
 var NativeNotification = function() {};
 
 NativeNotification.prototype.getToken = function (senderId, onSuccess, onFailed) {
-    exec(onSuccess, onFailed, 'NotificationPlugin', 'getToken', [{senderId: senderId}]);
+    exec(onSuccess, onFailed, 'NativeNotification', 'getToken', [{senderId: senderId}]);
 };
 
 module.exports = new NativeNotification();
